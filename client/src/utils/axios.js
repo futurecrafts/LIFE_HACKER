c7ac1eefe1f283c3a69ff5a94c4df8a8a1fab05a
YB <yb@yb.com>
2023-03-28 12:52:06 +1000

    update url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create(
     {
-        baseURL: 'http://localhost:7777',
+        baseURL: 'https://life-hacker.onrender.com:7777',
         withCredentials: true
     }
 );
