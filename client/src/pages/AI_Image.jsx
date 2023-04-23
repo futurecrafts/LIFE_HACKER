4364e9c0a9c7b7cf636bdca0a6ca14a21cf3db89
futurecrafts <biny38317@naver.com>
2023-04-24 08:00:33 +0900

    Update AI_Image.jsx

--- client/src/pages/AI_Image.jsx
+++ client/src/pages/AI_Image.jsx
@@ -42,7 +42,7 @@ export default function AI_Image() {
         setLoading(true);
 
         try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://ms-ai-image.onrender.com/api/v1/post', {
             method: 'GET',
             headers: {
             'Content-Type': 'application/json',
